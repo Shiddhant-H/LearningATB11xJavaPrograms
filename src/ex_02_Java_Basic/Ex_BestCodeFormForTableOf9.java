b2f42f095d89143a477f87f6ecb9d095a19b0b27
Lab001 <shiddhanth27@gmail.com>
2025-03-11 09:06:18 +0530

    11-march Fast Execution code with low memory used code push

---
+++ src/ex_02_Java_Basic/Ex_BestCodeFormForTableOf9.java
@@ -0,0 +1,18 @@
+package ex_02_Java_Basic;
+
+public class Ex_BestCodeFormForTableOf9 {
+    public static void main(String[] args) {
+        byte a = 9;
+        byte b = 1;
+        System.out.printf("%d x %d = %d \n",a,b,a*b, b++);
+        System.out.printf("%d x %d = %d \n",a,b,a*b, b++);
+        System.out.printf("%d x %d = %d \n",a,b,a*b, b++);
+        System.out.printf("%d x %d = %d \n",a,b,a*b, b++);
+        System.out.printf("%d x %d = %d \n",a,b,a*b, b++);
+        System.out.printf("%d x %d = %d \n",a,b,a*b, b++);
+        System.out.printf("%d x %d = %d \n",a,b,a*b, b++);
+        System.out.printf("%d x %d = %d \n",a,b,a*b, b++);
+        System.out.printf("%d x %d = %d \n",a,b,a*b, b++);
+        System.out.printf("%d x %d = %d \n",a,b,a*b, b++);
+    }
+}
---
+++ src/ex_02_Java_Basic/Ex_TableOf9Extra01WithoutLoop.java
@@ -0,0 +1,17 @@
+package ex_02_Java_Basic;
+
+public class Ex_TableOf9Extra01WithoutLoop {
+    public static void main(String[] args) {
+        byte table=9; //printing table of 9 with decimal value
+        System.out.printf("%d x %d = %d\n",table,1,table*1);
+        System.out.printf("%d x %d = %d\n",table,2,table*2);
+        System.out.printf("%d x %d = %d\n",table,3,table*3);
+        System.out.printf("%d x %d = %d\n",table,4,table*4);
+        System.out.printf("%d x %d = %d\n",table,5,table*5);
+        System.out.printf("%d x %d = %d\n",table,6,table*6);
+        System.out.printf("%d x %d = %d\n",table,7,table*7);
+        System.out.printf("%d x %d = %d\n",table,8,table*8);
+        System.out.printf("%d x %d = %d\n",table,9,table*9);
+        System.out.printf("%d x %d = %d",table,10,table*10);
+    }
+}
